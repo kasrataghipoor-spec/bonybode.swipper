5577bc88e9b8667719320ea727c231bdf31555a9
kasra t <kasra.taghipoor@gmail.com>
2026-06-02 16:09:07 +0330

    adding responsive for mobile part 1

--- src/app.js
+++ src/app.js
@@ -18,13 +18,41 @@ var swiperOne = new Swiper(".categorySwiper", {
   },
 });
 var swiperOne = new Swiper(".categorySwiper2", {
-  slidesPerView: 5,
-  spaceBetween: 150,
+  slidesPerView: "auto", // مهم: به جای عدد ثابت
+  spaceBetween: 12, // فاصله مناسب بین اسلایدها
   loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  // اضافه کردن breakpoints برای ریسپانسیو
+  breakpoints: {
+    // موبایل
+    0: {
+      slidesPerView: 1.2,
+      spaceBetween: 10,
+    },
+    // موبایل بزرگ
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 12,
+    },
+    // تبلت
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 15,
+    },
+    // دسکتاپ کوچک
+    1024: {
+      slidesPerView: 4.5,
+      spaceBetween: 20,
+    },
+    // دسکتاپ بزرگ
+    1280: {
+      slidesPerView: 5,
+      spaceBetween: 24,
+    },
+  }
 });
 var swiperOne = new Swiper(".readyCards", {
   slidesPerView: 4,
